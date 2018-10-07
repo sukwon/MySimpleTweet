39b5f2a61a02d8cf1859db56902bca4e9acf33a2
Sukwon Lee <sukwonlee231@gmail.com>
2018-10-06 20:53:21 -0700

    Add new tweet activity

---
+++ app/src/main/java/com/codepath/apps/restclienttemplate/activities/NewTweetActivity.java
@@ -0,0 +1,72 @@
+package com.codepath.apps.restclienttemplate.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+import com.codepath.apps.restclienttemplate.R;
+import com.codepath.apps.restclienttemplate.TwitterApp;
+import com.codepath.apps.restclienttemplate.models.User;
+import com.codepath.apps.restclienttemplate.network.TwitterClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import cz.msebera.android.httpclient.Header;
+
+public class NewTweetActivity extends AppCompatActivity {
+
+    private TwitterClient client;
+    private User user;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_tweet);
+
+        client = TwitterApp.getRestClient(this);
+    }
+
+    public void onClickTweetBtn(View view) {
+        EditText etTweetBody = findViewById(R.id.etTweetBody);
+        String body = String.valueOf(etTweetBody.getText());
+        client.postTweet(body, new JsonHttpResponseHandler() {
+
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                //This is the method I need to implement
+                Log.d("TwitterClient", response.toString());
+            }
+
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONArray response) {
+                Log.d("TwitterClient", response.toString());
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+                Log.d("TwitterClient", responseString);
+                throwable.printStackTrace();
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONArray errorResponse) {
+                Log.d("TwitterClient", errorResponse.toString());
+                throwable.printStackTrace();
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+                Log.d("TwitterClient", errorResponse.toString());
+                throwable.printStackTrace();
+            }
+        });
+    }
+
+    public void onClickCloseBtn(View view) {
+        finish();
+    }
+}
--- app/src/main/java/com/codepath/apps/restclienttemplate/activities/TimelineActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/activities/TimelineActivity.java
@@ -1,5 +1,6 @@
 package com.codepath.apps.restclienttemplate.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
@@ -10,10 +11,10 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 import com.codepath.apps.restclienttemplate.R;
-import com.codepath.apps.restclienttemplate.adapters.TweetAdapter;
 import com.codepath.apps.restclienttemplate.TwitterApp;
-import com.codepath.apps.restclienttemplate.network.TwitterClient;
+import com.codepath.apps.restclienttemplate.adapters.TweetAdapter;
 import com.codepath.apps.restclienttemplate.models.Tweet;
+import com.codepath.apps.restclienttemplate.network.TwitterClient;
 import com.loopj.android.http.JsonHttpResponseHandler;
 
 import org.json.JSONArray;
@@ -110,42 +111,36 @@ public class TimelineActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_new_tweet) {
-            //launch new tweet activity
-            client.postTweet("test", new JsonHttpResponseHandler() {
-
-                @Override
-                public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
-                    //This is the method I need to implement
-                    Log.d("TwitterClient", response.toString());
-                }
-
-                @Override
-                public void onSuccess(int statusCode, Header[] headers, JSONArray response) {
-                    Log.d("TwitterClient", response.toString());
-                }
-
-                @Override
-                public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
-                    Log.d("TwitterClient", responseString);
-                    throwable.printStackTrace();
-                }
-
-                @Override
-                public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONArray errorResponse) {
-                    Log.d("TwitterClient", errorResponse.toString());
-                    throwable.printStackTrace();
-                }
-
-                @Override
-                public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
-                    Log.d("TwitterClient", errorResponse.toString());
-                    throwable.printStackTrace();
-                }
-            });
+            launchArticleActivity();
             return true;
         }
 
         return super.onOptionsItemSelected(item);
     }
 
+    // Navigation
+
+    private final int NEW_TWEET_ACTIVITY_REQUEST_CODE = 20;
+
+    private void launchArticleActivity() {
+        Intent i = new Intent(getApplicationContext(), NewTweetActivity.class);
+        startActivityForResult(i, NEW_TWEET_ACTIVITY_REQUEST_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == RESULT_OK && requestCode == NEW_TWEET_ACTIVITY_REQUEST_CODE) {
+            Log.d("DEBUG", "onActivityResult");
+//            SearchFilter newFilter = (SearchFilter) data.getExtras().get(SearchFilter.id);
+//            filter = newFilter;
+//
+//            SearchView searchView = (SearchView) searchItem.getActionView();
+//            String query = searchView.getQuery().toString();
+//            if (query.isEmpty() == false) {
+//                articles.clear();
+//                adapter.notifyDataSetChanged();
+//                fetchArticles(1);
+//            }
+        }
+    }
 }
